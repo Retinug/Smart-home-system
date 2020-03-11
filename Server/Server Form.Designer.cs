42fa6bd290183313bdcd45099809ba16e8435a53
Retinug <Retinug@gmail.com>
2020-03-11 13:18:01 +0300

    Small interface changes

--- Server/Server Form.Designer.cs
+++ Server/Server Form.Designer.cs
@@ -30,8 +30,6 @@
         {
             this.text_Input = new System.Windows.Forms.TextBox();
             this.text_Console = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.list_Connections = new System.Windows.Forms.ListBox();
             this.button_Refresh = new System.Windows.Forms.Button();
             this.button_Disconnect = new System.Windows.Forms.Button();
             this.button_Connect = new System.Windows.Forms.Button();
@@ -42,7 +40,7 @@
             // text_Input
             // 
             this.text_Input.Location = new System.Drawing.Point(16, 414);
-            this.text_Input.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.text_Input.Margin = new System.Windows.Forms.Padding(4);
             this.text_Input.Name = "text_Input";
             this.text_Input.Size = new System.Drawing.Size(372, 22);
             this.text_Input.TabIndex = 3;
@@ -53,7 +51,7 @@
             this.text_Console.BackColor = System.Drawing.SystemColors.Desktop;
             this.text_Console.ForeColor = System.Drawing.Color.Lime;
             this.text_Console.Location = new System.Drawing.Point(16, 12);
-            this.text_Console.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.text_Console.Margin = new System.Windows.Forms.Padding(4);
             this.text_Console.Multiline = true;
             this.text_Console.Name = "text_Console";
             this.text_Console.ReadOnly = true;
@@ -61,31 +59,10 @@
             this.text_Console.Size = new System.Drawing.Size(372, 393);
             this.text_Console.TabIndex = 4;
             // 
-            // button2
-            // 
-            this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(407, 306);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(160, 28);
-            this.button2.TabIndex = 12;
-            this.button2.Text = "Disconnect";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // list_Connections
-            // 
-            this.list_Connections.FormattingEnabled = true;
-            this.list_Connections.ItemHeight = 16;
-            this.list_Connections.Location = new System.Drawing.Point(407, 214);
-            this.list_Connections.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.list_Connections.Name = "list_Connections";
-            this.list_Connections.Size = new System.Drawing.Size(160, 84);
-            this.list_Connections.TabIndex = 11;
-            // 
             // button_Refresh
             // 
             this.button_Refresh.Location = new System.Drawing.Point(407, 178);
-            this.button_Refresh.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button_Refresh.Margin = new System.Windows.Forms.Padding(4);
             this.button_Refresh.Name = "button_Refresh";
             this.button_Refresh.Size = new System.Drawing.Size(160, 28);
             this.button_Refresh.TabIndex = 10;
@@ -97,7 +74,7 @@
             // 
             this.button_Disconnect.Enabled = false;
             this.button_Disconnect.Location = new System.Drawing.Point(407, 143);
-            this.button_Disconnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button_Disconnect.Margin = new System.Windows.Forms.Padding(4);
             this.button_Disconnect.Name = "button_Disconnect";
             this.button_Disconnect.Size = new System.Drawing.Size(160, 28);
             this.button_Disconnect.TabIndex = 9;
@@ -109,7 +86,7 @@
             // 
             this.button_Connect.Enabled = false;
             this.button_Connect.Location = new System.Drawing.Point(407, 107);
-            this.button_Connect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button_Connect.Margin = new System.Windows.Forms.Padding(4);
             this.button_Connect.Name = "button_Connect";
             this.button_Connect.Size = new System.Drawing.Size(160, 28);
             this.button_Connect.TabIndex = 8;
@@ -122,7 +99,7 @@
             this.list_Port.FormattingEnabled = true;
             this.list_Port.ItemHeight = 16;
             this.list_Port.Location = new System.Drawing.Point(407, 15);
-            this.list_Port.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.list_Port.Margin = new System.Windows.Forms.Padding(4);
             this.list_Port.Name = "list_Port";
             this.list_Port.Size = new System.Drawing.Size(160, 84);
             this.list_Port.TabIndex = 7;
@@ -144,8 +121,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(576, 444);
             this.Controls.Add(this.labelIP);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.list_Connections);
             this.Controls.Add(this.button_Refresh);
             this.Controls.Add(this.button_Disconnect);
             this.Controls.Add(this.button_Connect);
@@ -153,6 +128,7 @@
             this.Controls.Add(this.text_Console);
             this.Controls.Add(this.text_Input);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
             this.Name = "Server_Form";
             this.Text = "Server";
             this.ResumeLayout(false);
@@ -161,8 +137,6 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ListBox list_Connections;
         private System.Windows.Forms.Button button_Refresh;
         private System.Windows.Forms.Label labelIP;
         public System.Windows.Forms.Button button_Disconnect;
